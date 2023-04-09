cf644a165b05afa8a4607b0845c4eccb0372c5d4
akitaSummer <644171127@qq.com>
2023-04-10 02:49:24 +0800

    fix: clear code

--- src/cli/open.rs
+++ src/cli/open.rs
@@ -1,10 +1,8 @@
-use std::{cell::RefCell, env, rc::Rc};
+use std::{env, rc::Rc};
 
 use structopt::StructOpt;
 
 use crate::{
-    core::window::Window,
-    javascript::{JsRuntime, JsRuntimeState},
     ui::views::{init_menu, BrowserView},
     utils,
 };
@@ -22,29 +20,9 @@ pub fn run(opts: Opts) {
 
     print!("url is : {}\n", start_url);
 
-    let mut runtime = JsRuntime::new(None);
-
-    let window = Rc::new(RefCell::new(Window {
-        name: "akitasummer".to_string(),
-    }));
-
-    JsRuntimeState::set_window(&mut runtime.isolate, window);
-
     let mut siv = cursive::default();
 
     init_menu(&mut siv);
-
-    // let code = r#"
-    // async function hello_world() {
-    //     print(window.name);
-    //     window.name = "test"
-    //     print(window.name);
-    // }
-    // hello_world();
-    // "#;
-
-    // let result = runtime.execute_script(code);
-    // println!("Result is: {:#?}", result);
 
     let mut b = BrowserView::named(Rc::new(siv.cb_sink().clone()));
     b.get_mut().navigate_to(start_url);
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use structopt::StructOpt;
 fn main() {
     let opts = cli::opts::Command::from_args();
 
-    let exit_code = match opts {
+    match opts {
         cli::opts::Command::Open(opt) => cli::open::run(opt),
     };
-
-    // std::process::exit(exit_code)
 }
